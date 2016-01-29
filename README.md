054cc5c4e03cb9c2a76b97732ff38a149ef2abd1
ZhangLiang <zhangliang@21cake.com>
2016-01-29 11:07:57 -0800

    fix execute path

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CS599-Robot-P2
+
+* How to compile?
+1. go to "control" folder
+2. make all
+
+* How to run the robots?
+1. go to "control" folder
+2. sh startwalk.sh
--- control/startwalk.sh
+++ control/startwalk.sh
@@ -1,6 +1,6 @@
-./safewalk -p 6666 &
-./safewalk -p 6667 &
-./safewalk -p 6668 &
-./safewalk -p 6669 &
-./safewalk -p 6670 &
-./safewalk -r l -p 6665
+build/safewalk -p 6666 &
+build/safewalk -p 6667 &
+build/safewalk -p 6668 &
+build/safewalk -p 6669 &
+build/safewalk -p 6670 &
+build/safewalk -r l -p 6665
